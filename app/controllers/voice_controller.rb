a38ee3f1d12221b4d3139b42f50aa9863a0b85b8
Will Read <will.read@gmail.com>
2016-04-23 15:57:02 -0700

    switch for testing vs. Monday

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -3,14 +3,19 @@ class VoiceController < ApplicationController
   after_filter :set_header
   skip_before_action :verify_authenticity_token
 
-  WILLS_CELL = "415-894-9455"
+  WILLS_CELL = "4158949455"
 
   def start
     response = Twilio::TwiML::Response.new do |response|
-      response.Gather(numDigits: '4', timeout: '10', action: check_voice_path, method: 'get') do |gather|
-        gather.Say "Please enter your four digit code, or please stay on the line", voice: 'woman'
+      if(params["Caller"].include?(WILLS_CELL))
+        response.Redirect(real_human_voice_path, method: 'get')
+      else
+        response.Gather(numDigits: '4', timeout: '10', action: check_voice_path, method: 'get') do |gather|
+          gather.Say "Please enter your four digit code, or please stay on the line", voice: 'woman'
+        end
+        response.Redirect(real_human_voice_path, method: 'get')
       end
-      response.Redirect(real_human_voice_path, method: 'get')
+
     end
 
     render_twiml response
