d4830d6e0cd11f64bd03396fe63e7520ac72cc3a
Will Read <will.read@gmail.com>
2016-04-23 10:53:26 -0700

    new message

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -20,7 +20,7 @@ class VoiceController < ApplicationController
   def check
     return redirect_to start_voice_path unless params['Digits'] == "1234"
     response = Twilio::TwiML::Response.new do |response|
-      response.Play '/mp3/dial_tone_9_long.mp3'
+      response.Play(digits:"wwww9")
     end
 
     render_twiml response
