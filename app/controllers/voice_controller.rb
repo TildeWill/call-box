bb3e24e89a8bc9781f2d77366ef55c8bdef9a27a
Will Read <will.read@gmail.com>
2016-05-03 19:16:19 -0700

    Use real call box instead of passthru

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -7,14 +7,10 @@ class VoiceController < ApplicationController
 
   def start
     response = Twilio::TwiML::Response.new do |response|
-      if(params["Caller"].include?(WILLS_CELL.gsub("-", '')))
-        response.Gather(numDigits: '4', timeout: '10', action: check_voice_path, method: 'get') do |gather|
-          gather.Say "To buzz in, please enter your four digit code, or stay on the line", voice: 'woman'
-        end
-        response.Redirect(real_human_voice_path, method: 'get')
-      else
-        response.Redirect(real_human_voice_path, method: 'get')
+      response.Gather(numDigits: '4', timeout: '10', action: check_voice_path, method: 'get') do |gather|
+        gather.Say "To buzz in, please enter your four digit code, or stay on the line", voice: 'woman'
       end
+      response.Redirect(real_human_voice_path, method: 'get')
     end
 
     render_twiml response
